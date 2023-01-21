1a9e283a78b80aaeee2fa5af2ae1d008c660bdf4
Jorel Huerto <jjhue29@gmail.com>
2023-01-20 22:48:29 -0800

    nvm didnt need this

--- Assets/Resources/Semi-Active Homing Fish/Scripts/SemiActiveHomingFishWeaponController.cs
+++ Assets/Resources/Semi-Active Homing Fish/Scripts/SemiActiveHomingFishWeaponController.cs
@@ -5,24 +5,6 @@ using UnityEngine;
 public class SAHFishWeaponController : MonoBehaviour
 {
     public Transform homingTarget;
-    //following code
-    //modified from Sebasian Lague -
-    //Field of View Visualization
-    //https://www.youtube.com/watch?v=rQG9aUWarwE
-    Rigidbody weaponRigidbody;
-    Camera viewCamera;
-
-    void Start()
-    {
-        
-        weaponRigidbody = GetComponent<Rigidbody>();
-        viewCamera = PlayerController.Instance.GetComponent<Camera>();
-
-    }
-
-    void Update()
-    {
-        Vector3 mouseLook = viewCamera.ScreenToWorldPoint(new Vector3 (Input.mousePosition.x, Input.mousePosition.y, viewCamera.transform.position.y));
-    }
+   
 
 }
